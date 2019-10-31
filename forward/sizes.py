1ff439de649b94f7b1c3a6344155682f8af9b7fb
Lucia Fonseca <55983939+Lucia-Fonseca@users.noreply.github.com>
2019-10-31 14:57:02 +0000

    Minor changes

--- forward/sizes.py
+++ forward/sizes.py
@@ -8,7 +8,7 @@ import numpy as np
 # The mean is given by equation [3.14]
 # Free parameters: amu, bmu and sigma
 
-def rphys50(M, amu = 1, bmu = 0, sigma = 1, size = 100):
+def rphys50(M, amu = 1, bmu = 0, sigma = 1, size = None):
     muphys = amu * M + bmu
     r = np.random.lognormal(muphys, sigma, size)
     return r
@@ -19,5 +19,5 @@ def rphys50(M, amu = 1, bmu = 0, sigma = 1, size = 100):
 
 def rang50(r = 1, z = 0, c):
     dA = c.angular_diameter_distance(z)
-    ang = r * dA
+    ang = r / dA
     return ang
